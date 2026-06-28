8a8f5e2cd5829c358ecb3f23f83aac72c6e68ad8
Genesis <izukarotechi@gmail.com>
2026-06-28 02:16:57 +0100

    Make CMS-backed routes dynamic to fix build-time Payload init

--- src/app/(app)/(site)/[...slug]/page.tsx
+++ src/app/(app)/(site)/[...slug]/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic'
 import { notFound } from 'next/navigation'
 import { draftMode } from 'next/headers'
 import type { Metadata } from 'next'
--- src/app/(app)/(site)/careers/page.tsx
+++ src/app/(app)/(site)/careers/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic'
 import type { Metadata } from 'next'
 import { getPayload } from 'payload'
 import configPromise from '@payload-config'
